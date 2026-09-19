ed5b689d159446b65148aa01e81edcbd545f1bea
tomjerrygood <109782843+tomjerrygood@users.noreply.github.com>
2026-09-19 15:53:58 +0800

    Refactor build-dropbear-android.sh script
    Refactor build script for Dropbear on Android, including downloading source, applying patches, and configuring for ARM architecture.

--- build-dropbear-android.sh
+++ build-dropbear-android.sh
@@ -1,87 +1,52 @@
 #!/bin/bash
-
 set -e
 
-if [ -z ${TOOLCHAIN} ]; then echo "TOOLCHAIN must be set. See README.md for more information."; exit -1; fi
-
-# Setup the environment
-export TARGET=../target
-# Specify binaries to build. Options: dropbear dropbearkey scp dbclient
-export PROGRAMS="dropbear dropbearkey"
-# Which version of Dropbear to download for patching
-export VERSION=2018.76
-
-# Download the latest version of dropbear SSH
-if [ ! -f ./dropbear-$VERSION.tar.bz2 ]; then
-    wget -O ./dropbear-$VERSION.tar.bz2 https://matt.ucc.asn.au/dropbear/releases/dropbear-$VERSION.tar.bz2
-fi
-
-# Start each build with a fresh source copy
-rm -rf ./dropbear-$VERSION
-tar xjf dropbear-$VERSION.tar.bz2
-
-# Change to dropbear directory
-cd dropbear-$VERSION
-
-### START -- configure without modifications first to generate files 
-#########################################################################################################################
-echo "Generating required files..."
-
+VERSION=2018.76
+PREFIX=/tmp/dropbear-android
 HOST=arm-linux-androideabi
-COMPILER=${TOOLCHAIN}/bin/arm-linux-androideabi-gcc
-STRIP=${TOOLCHAIN}/bin/arm-linux-androideabi-strip
-SYSROOT=${TOOLCHAIN}/sysroot
 
-export CC="$COMPILER --sysroot=$SYSROOT"
+# 使用GitHub Action传入的TOOLCHAIN路径
+export TOOLCHAIN="$TOOLCHAIN"
+export PATH="$TOOLCHAIN/bin:$PATH"
 
-# Android 5.0 Lollipop and greater require PIE. Default to this unless otherwise specified.
-if [ -z $DISABLE_PIE ]; then export CFLAGS="-g -O2 -pie -fPIE"; else echo "Disabling PIE compilation..."; fi
-sleep 5
-# Use the default platform target for pie binaries 
-unset GOOGLE_PLATFORM
+# 下载原版dropbear源码
+wget -q https://mjt.dl.sourceforge.net/project/dropbear/dropbear-${VERSION}.tar.bz2
 
-# Apply the new config.guess and config.sub now so they're not patched
-cp ../config.guess ../config.sub .
-    
-./configure --host=$HOST --disable-utmp --disable-wtmp --disable-utmpx --disable-zlib --disable-syslog > /dev/null 2>&1
+# 解压源码
+tar -xjf dropbear-${VERSION}.tar.bz2
 
-echo "Done generating files"
-sleep 2
-echo
-echo
-#########################################################################################################################
-### END -- configure without modifications first to generate files 
-
-# Begin applying changes to make Android compatible
-# Apply the compatibility patch
-patch -p1 < ../android-compat.patch
+# ====================== 关键：先打补丁，再configure ======================
+cd dropbear-${VERSION}
+# 打安卓兼容补丁，CI非交互模式
+patch -p1 -N --no-backup < ../android-compat.patch
 cd -
 
-echo "Compiling for ARM"  
+# 进入源码目录执行configure编译配置
+cd dropbear-${VERSION}
 
-cd dropbear-$VERSION
-    
-./configure --host=$HOST --disable-utmp --disable-wtmp --disable-utmpx --disable-zlib --disable-syslog
+./configure \
+  --host=${HOST} \
+  --prefix=${PREFIX} \
+  --disable-zlib \
+  --enable-static \
+  --disable-shadow \
+  --disable-utmp \
+  --disable-pty \
+  --disable-syslog \
+  --disable-lastlog
 
-make PROGRAMS="$PROGRAMS"
-MAKE_SUCCESS=$?
-if [ $MAKE_SUCCESS -eq 0 ]; then
-	clear
-	sleep 1
-  	# Create the output directory
-	mkdir -p $TARGET/arm;
-	for PROGRAM in $PROGRAMS; do
+echo "===== Start make ====="
+make -j$(nproc)
+make install
 
-		if [ ! -f $PROGRAM ]; then
-    		echo "${PROGRAM} not found!"
-		fi
+# 拷贝编译好的二进制到项目输出目录
+mkdir -p ../target/arm
+cp ${PREFIX}/sbin/dropbear ../target/arm/
+cp ${PREFIX}/bin/dropbearkey ../target/arm/
 
-		$STRIP "./${PROGRAM}"
-	done
+# strip裁剪体积，去掉调试符号
+${HOST}-strip ../target/arm/dropbear
+${HOST}-strip ../target/arm/dropbearkey
 
-	cp $PROGRAMS $TARGET/arm
-	echo "Compilation successful. Output files are located in: ${TARGET}/arm"
-else
- 	echo "Compilation failed."
-fi
-
+echo "Build done, binaries in target/arm/"
+ls -lh ../target/arm/
